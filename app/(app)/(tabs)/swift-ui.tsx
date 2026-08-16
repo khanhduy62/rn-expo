43383c255b6c3962a7e36b31b5d0bce58d4855b1
Duy Le <lekhanhduyduy1201@gmail.com>
2026-08-16 20:40:18 +0700

    add modifier

--- app/(app)/(tabs)/swift-ui.tsx
+++ app/(app)/(tabs)/swift-ui.tsx
@@ -1,11 +1,23 @@
+import { mySwiftUiViewSwiftUIModifier } from "@/modules/my-swift-ui-view/src/MySwiftUiViewSwiftUIModifier";
 import MySwiftUiViewSwiftUIView from "@/modules/my-swift-ui-view/src/MySwiftUiViewSwiftUIView";
 import { Host } from "@expo/ui/swift-ui";
+import { padding } from "@expo/ui/swift-ui/modifiers";
 
 export default function LoadingView() {
   return (
     <Host style={{ flex: 1 }}>
       <MySwiftUiViewSwiftUIView
         title="Hello from SwiftUI!"
+        modifiers={[
+          padding({
+            all: 20,
+          }),
+          mySwiftUiViewSwiftUIModifier({
+            color: "green",
+            width: 1,
+            cornerRadius: 10,
+          }),
+        ]}
         onTap={() => {
           alert("SwiftUI view tapped!");
         }}
