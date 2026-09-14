40264c96fdde440385bfe70bc2fb57793728bc3c
Duy Le <lekhanhduyduy1201@gmail.com>
2026-09-15 00:29:52 +0700

    environment variables

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -19,7 +19,10 @@ export default function Index() {
       }}
     >
       <Text testID="home-screen">Home Screen</Text>
-      <Text>Expo ENV: {process.env.EXPO_PUBLIC_API_URL}</Text>
+      <Text>
+        Expo ENV: {process.env.EXPO_PUBLIC_API_BASE_URL} (
+        {process.env.EXPO_PUBLIC_VARIABLE})
+      </Text>
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <Link href="/deeplinking/1">Go to deeplinking details</Link>
       <Link href="/profile">Go to profile</Link>
@@ -56,18 +59,46 @@ export default function Index() {
 }
 
 const jsonKeys = {
-  "name":"rn-expo",
-  "slug":"rn-expo",
-  "version":"1.0.0",
-  "orientation":"portrait",
-  "icon":"./assets/images/icon.png",
-  "scheme":"rnexpo",
-  "userInterfaceStyle":"automatic",
-  "newArchEnabled":true,
-  "ios":{"supportsTablet":true,"bundleIdentifier":"com.dle-radicle.rn-expo"},
-  "android":{"adaptiveIcon":{"backgroundColor":"#E6F4FE","foregroundImage":"./assets/images/android-icon-foreground.png","backgroundImage":"./assets/images/android-icon-background.png","monochromeImage":"./assets/images/android-icon-monochrome.png"},"predictiveBackGestureEnabled":false,"package":"com.dleradicle.rnexpo"},
-  "web":{"output":"static","favicon":"./assets/images/favicon.png"},
-  "plugins":["expo-router",["expo-splash-screen",{"image":"./assets/images/splash-icon.png","imageWidth":200,"resizeMode":"contain","backgroundColor":"#ffffff","dark":{"backgroundColor":"#000000"}}],"expo-font","expo-image","expo-status-bar","expo-web-browser"],
-  "experiments":{"typedRoutes":true,"reactCompiler":true},
-  "extra":{"router":{},"eas":{"projectId":"3b299ab8-e18a-4ecd-9287-9f433c3a2265"}}
-}
+  name: "rn-expo",
+  slug: "rn-expo",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "rnexpo",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: { supportsTablet: true, bundleIdentifier: "com.dle-radicle.rn-expo" },
+  android: {
+    adaptiveIcon: {
+      backgroundColor: "#E6F4FE",
+      foregroundImage: "./assets/images/android-icon-foreground.png",
+      backgroundImage: "./assets/images/android-icon-background.png",
+      monochromeImage: "./assets/images/android-icon-monochrome.png",
+    },
+    predictiveBackGestureEnabled: false,
+    package: "com.dleradicle.rnexpo",
+  },
+  web: { output: "static", favicon: "./assets/images/favicon.png" },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+        dark: { backgroundColor: "#000000" },
+      },
+    ],
+    "expo-font",
+    "expo-image",
+    "expo-status-bar",
+    "expo-web-browser",
+  ],
+  experiments: { typedRoutes: true, reactCompiler: true },
+  extra: {
+    router: {},
+    eas: { projectId: "3b299ab8-e18a-4ecd-9287-9f433c3a2265" },
+  },
+};
